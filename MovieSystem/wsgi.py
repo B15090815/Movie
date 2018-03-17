072382e2166b03a0c78b80c5b1d07b890e8bbb5a
crx <940909850@qq.com>
2018-03-17 20:12:05 +0800

    添加apache配置文件

--- MovieSystem/wsgi.py
+++ MovieSystem/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieSystem.settings")
 
 application = get_wsgi_application()
+
+
+
+
+from os.path import join,dirname,abspath
+import sys
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
+
+
